e2ddbdd93f046f9993b6bc79498c5e5ef8db406f
Cristiano Teixeira <prof.cristiano.info@gmail.com>
2019-09-02 21:16:31 -0300

    Update portugolc.cpp

--- portugolc.cpp
+++ portugolc.cpp
@@ -1,48 +1,30 @@
-#include "Arduino.h"
-#include "Servo.h"
-#include "IoT.h"
+#include "portugolc.h"
+#include <iostream>
+#include <string>
+#include<time.h>
+#include <locale.h>
 
-void defineLed(int led){
-    pinMode(led, OUTPUT);
+
+
+void espera(unsigned long mseconds){
+	clock_t goal = mseconds + clock();
+    while (goal > clock());;
 }
-
-void acendeLed(int pin){
-	digitalWrite(pin, HIGH);
+void brazil (){
+	setlocale(LC_ALL,"");
 }
-
-void apagaLed(int pin){
-	digitalWrite(pin, LOW);
-}
-
-void brilhaLed(int pin, int brilho){
-	analogWrite(pin, brilho);
-}
-
-void espera(long time){
-	delay(time);
+void limpatela(){
+	system("cls");
 }
 
-void botao(int pin){
-	pinMode(pin, INPUT_PULLUP);
-}
-
-uint8_t verificaBotao(int pin){
-	return digitalRead(pin);
-}
-
-int botaoLigado(int pin){
-	return (digitalRead(pin) == LOW);
-}
-
-int botaoDesligado(int pin){
-	return (digitalRead(pin) == HIGH);
-}
-uint8_t Motor::conectar(int pin){
-    Servo::attach(pin);
-}
-void Motor::girar(int graus){
-    Servo::write(graus);
+void cor (char F, L)
+{
+	string cm;
+	//str(cm)
+	//system("color ","F","L")
 }
 
 
-
+void pausa(){
+	system ("pause")
+}
